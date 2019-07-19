d15758626a93c663357524cae50d9f2cad6e26ae
Noble Obioma <nobioma1@gmail.com>
2019-07-19 11:10:19 +0100

    Create Routes to add, update and delete for actions

--- api/actions/actionRouter.js
+++ api/actions/actionRouter.js
@@ -1,13 +1,37 @@
 const express = require('express');
-const {} = require('./actionModel.js');
+const { addAction, updateAction, removeAction } = require('./actionModel');
+const { validateActionId, validateAction } = require('./actionMiddleware');
 
 const actionRouter = express.Router();
 
-actionRouter.get('/', async (req, res) => {
+actionRouter.post('/:projectId', validateAction, async (req, res) => {
+  const projectId = req.params.projectId;
   try {
+    const action = await addAction(projectId, req.body);
+    res.status(201).json(action);
   } catch (error) {
-    res.status(500).json({ error: '' });
+    res.status(500).json({ error: 'Error adding Action' });
   }
 });
 
-module.exports = actionRouter
+actionRouter.put('/:id', validateActionId, validateAction, async (req, res) => {
+  const { id } = req.action;
+
+  try {
+    const actionId = await updateAction(id, req.body);
+    return res.status(200).json(actionId);
+  } catch (error) {
+    return res.status(500).json({ error: 'Error updating action' });
+  }
+});
+
+actionRouter.delete('/:id', validateActionId, async (req, res) => {
+  try {
+    await removeAction(req.action.id);
+    return res.status(204).end();
+  } catch (error) {
+    return res.status(500).json({ error: 'Error deleting action' });
+  }
+});
+
+module.exports = actionRouter;
