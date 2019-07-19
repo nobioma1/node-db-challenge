80c3b662a11ac547dcf34486a9ffb7c8182ddf94
Noble Obioma <nobioma1@gmail.com>
2019-07-19 08:25:47 +0100

    Create db config

--- api/server.js
+++ api/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const projectRouter = require('./projects/projectRouter');
+const actionRouter = require('./actions/actionRouter');
 
 const server = express();
 server.use(express.json());
+
+server.use('/api/projects/', projectRouter);
+server.use('/api/actions/', actionRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to PROAC API' });
