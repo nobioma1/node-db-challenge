c7e9bd0b76287013629a95311764e8e237ad07d4
Noble Obioma <nobioma1@gmail.com>
2019-07-19 08:25:21 +0100

    Update sqlite name in init file

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/databaseProjAct.db3'
+      filename: './data/databaseProAct.db3'
     },
     useNullAsDefault: true,
     pool: {
